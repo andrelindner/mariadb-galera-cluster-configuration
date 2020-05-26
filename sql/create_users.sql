3929068894b03096367a58898b850244b10377b6
André Lindner <4486091+andrelindner@users.noreply.github.com>
2020-05-26 16:46:43 +0200

    Update create_users.sql

--- sql/create_users.sql
+++ sql/create_users.sql
@@ -1,26 +1,9 @@
 # Example for node 1 - localhost = socket
 # Please change the ip-addresses
 CREATE USER 'galerasync'@'192.168.1.11' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'192.168.1.11';
+GRAND ALL PRIVILIGES ON *.* TO 'galerasync'@'192.168.1.11';
 CREATE USER 'galerasync'@'192.168.1.12' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'192.168.1.12';
-CREATE USER 'galerasync'@'localhost' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'localhost';
+GRAND ALL PRIVILIGES ON *.* TO 'galerasync'@'192.168.1.12';
+CREATE USER 'galerasync'@'192.168.1.13' IDENTIFIED BY '<password>';
+GRAND ALL PRIVILIGES ON *.* TO 'galerasync'@'192.168.1.13';
 
-# Example for node 2 - localhost = socket
-# Please change the ip-addresses
-CREATE USER 'galerasync'@'192.168.1.10' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'192.168.1.10';
-CREATE USER 'galerasync'@'192.168.1.12' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'192.168.1.12';
-CREATE USER 'galerasync'@'localhost' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'localhost';
-
-# Example for node 3 - localhost = socket
-# Please change the ip-addresses
-CREATE USER 'galerasync'@'192.168.1.10' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'192.168.1.10';
-CREATE USER 'galerasync'@'192.168.1.11' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'192.168.1.11';
-CREATE USER 'galerasync'@'localhost' IDENTIFIED BY '<password>';
-GRAND ALL PRIVILIGES ON *.* TO 'galerasymc'@'localhost';
