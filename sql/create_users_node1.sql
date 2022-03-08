e566895034037a181f2bc8878f605ee91904025c
André Lindner <4486091+andrelindner@users.noreply.github.com>
2022-03-08 11:40:41 +0100

    Update and rename create_users.sql to create_users_node1.sql

--- sql/create_users_node1.sql
+++ sql/create_users_node1.sql
@@ -1,9 +1,9 @@
 # Example for node 1 - localhost = socket
 # Please change the ip-addresses
+
 CREATE USER 'galerasync'@'192.168.1.1' IDENTIFIED BY '<password>';
 GRANT ALL PRIVILEGES ON *.* TO 'galerasync'@'192.168.1.1';
 CREATE USER 'galerasync'@'192.168.1.2' IDENTIFIED BY '<password>';
 GRANT ALL PRIVILEGES ON *.* TO 'galerasync'@'192.168.1.2';
 CREATE USER 'galerasync'@'192.168.1.3' IDENTIFIED BY '<password>';
 GRANT ALL PRIVILEGES ON *.* TO 'galerasync'@'192.168.1.3';
-
