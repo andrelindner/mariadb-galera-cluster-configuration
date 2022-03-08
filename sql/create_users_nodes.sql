7ab75c7955c264f517fd37f1e9bee59d3792589f
André Lindner <4486091+andrelindner@users.noreply.github.com>
2022-03-08 13:41:50 +0100

    Update create_users_nodes.sql

--- sql/create_users_nodes.sql
+++ sql/create_users_nodes.sql
@@ -7,3 +7,5 @@ CREATE USER 'galerasync'@'192.168.1.2' IDENTIFIED BY '<password>';
 GRANT ALL PRIVILEGES ON *.* TO 'galerasync'@'192.168.1.2';
 CREATE USER 'galerasync'@'192.168.1.3' IDENTIFIED BY '<password>';
 GRANT ALL PRIVILEGES ON *.* TO 'galerasync'@'192.168.1.3';
+CREATE USER 'galerasync'@'localhost' IDENTIFIED BY '<password>';
+GRANT ALL PRIVILEGES ON *.* TO 'galerasync'@'localhost';
